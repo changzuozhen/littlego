a44abdfeb1f27d0070b98385e28505c6f7ff7060
Patrick Näf <herzbube@herzbube.ch>
2013-04-08 00:31:43 +0200

    unfinished changes to fix up zoom scales after a zoom

--- src/play/scrollview/PlayViewScrollController.m
+++ src/play/scrollview/PlayViewScrollController.m
@@ -28,9 +28,11 @@
 
 @property(nonatomic, assign) UIScrollView* playViewScrollView;
 @property(nonatomic, assign) PlayView* playView;
+@property(nonatomic, assign) PlayViewModel* playViewModel;
 /// @brief The overall zoom scale currently in use for drawing the Play view.
 /// At zoom scale value 1.0 the entire board is visible.
 @property(nonatomic, assign) CGFloat currentAbsoluteZoomScale;
+@property(nonatomic, assign) CGSize playViewOriginalSize;
 @end
 
 
@@ -51,11 +53,14 @@
     return nil;
   self.playViewScrollView = scrollView;
   self.playView = playView;
+  self.playViewModel = [ApplicationDelegate sharedDelegate].playViewModel;
+  self.playViewOriginalSize = self.playView.frame.size;
   [self setupScrollViews];
-
-  PlayViewModel* playViewModel = [ApplicationDelegate sharedDelegate].playViewModel;
-  [playViewModel addObserver:self forKeyPath:@"maximumZoomScale" options:0 context:NULL];
-
+  [self.playViewModel addObserver:self forKeyPath:@"maximumZoomScale" options:0 context:NULL];
+  [[NSNotificationCenter defaultCenter] addObserver:self
+                                           selector:@selector(deviceOrientationChanged:)
+                                               name:UIDeviceOrientationDidChangeNotification
+                                             object:nil];
   return self;
 }
 
@@ -64,8 +69,9 @@
 // -----------------------------------------------------------------------------
 - (void) dealloc
 {
-  PlayViewModel* playViewModel = [ApplicationDelegate sharedDelegate].playViewModel;
-  [playViewModel removeObserver:self forKeyPath:@"maximumZoomScale"];
+  [[NSNotificationCenter defaultCenter] removeObserver:self];
+  [self.playViewModel removeObserver:self forKeyPath:@"maximumZoomScale"];
+  self.playViewModel = nil;
   [super dealloc];
 }
 
@@ -85,15 +91,30 @@
   self.playView.coordinateLabelsLetterViewScrollView.delegate = self;
   self.playView.coordinateLabelsNumberViewScrollView.delegate = self;
 
-  PlayViewModel* playViewModel = [ApplicationDelegate sharedDelegate].playViewModel;
   self.playViewScrollView.zoomScale = 1.0f;
   self.playViewScrollView.minimumZoomScale = 1.0f;
-  self.playViewScrollView.maximumZoomScale = playViewModel.maximumZoomScale;
+  self.playViewScrollView.maximumZoomScale = self.playViewModel.maximumZoomScale;
   [self synchronizeZoomScale:self.playViewScrollView.zoomScale
             minimumZoomScale:self.playViewScrollView.minimumZoomScale
             maximumZoomScale:self.playViewScrollView.maximumZoomScale];
 
+  NSLog(@"setup: content size w = %f, h = %f",
+        self.playViewScrollView.contentSize.width, self.playViewScrollView.contentSize.height);
+
   self.currentAbsoluteZoomScale = 1.0f;
+}
+
+// -----------------------------------------------------------------------------
+/// @brief Responds to UIDeviceOrientationDidChangeNotification.
+// -----------------------------------------------------------------------------
+- (void) deviceOrientationChanged:(NSNotification*)notification
+{
+  // TODO xxx The view may be zoomed when this notification occurs, in which
+  // case we will get the wrong size. A possible solution could be to have a
+  // transparent view that is set up with the original PlayView size and is
+  // then left untouched except for obtaining its sizes after it is rotated
+  // by the system. This is an incredibly clunky solution, though :-(
+  self.playViewOriginalSize = self.playView.frame.size;
 }
 
 // -----------------------------------------------------------------------------
@@ -148,15 +169,53 @@
 // -----------------------------------------------------------------------------
 - (void) scrollViewDidEndZooming:(UIScrollView*)scrollView withView:(UIView*)view atScale:(float)scale
 {
+  // Determine scroll view properties that we need to re-apply after the zoom
+  // scale is reset to 1.0. At this point we also make sure that rounding
+  // errors do not cause the minimum/maximum zoom scales to be exceeded
+  CGFloat newMinimumZoomScale;
+  CGFloat newMaximumZoomScale;
+  CGPoint newContentOffset;
+  CGSize newContentSize;
   self.currentAbsoluteZoomScale *= scale;
+  if (self.currentAbsoluteZoomScale < 1.0)
+  {
+    self.currentAbsoluteZoomScale = 1.0;
+    newMinimumZoomScale = 1.0;
+    newMaximumZoomScale = self.playViewModel.maximumZoomScale;
+    newContentOffset.x = 0.0;
+    newContentOffset.y = 0.0;
+    newContentSize = self.playViewOriginalSize;
+  }
+  else if (self.currentAbsoluteZoomScale > self.playViewModel.maximumZoomScale)
+  {
+    self.currentAbsoluteZoomScale = self.playViewModel.maximumZoomScale;
+    newMinimumZoomScale = 1.0 / self.playViewModel.maximumZoomScale;
+    newMaximumZoomScale = 1.0;
+    newContentSize.width = self.playViewOriginalSize.width * self.playViewModel.maximumZoomScale;
+    newContentSize.height = self.playViewOriginalSize.height * self.playViewModel.maximumZoomScale;
+    newContentOffset = scrollView.contentOffset;
+    if (newContentOffset.x + scrollView.frame.size.width > newContentSize.width)
+      newContentOffset.x = newContentSize.width - scrollView.frame.size.width;
+    if (newContentOffset.y + scrollView.frame.size.height > newContentSize.height)
+      newContentOffset.y = newContentSize.height - scrollView.frame.size.height;
+  }
+  else
+  {
+    newMinimumZoomScale = scrollView.minimumZoomScale / scale;
+    newMaximumZoomScale = scrollView.maximumZoomScale / scale;
+    newContentOffset = scrollView.contentOffset;
+    newContentSize = scrollView.contentSize;
+  }
   DDLogVerbose(@"scrollViewDidEndZooming: new overall zoom scale = %f", self.currentAbsoluteZoomScale);
 
   // Remember content offset and size so that we can re-apply them after we
   // reset the zoom scale to 1.0
+/*xxx
   CGPoint contentOffset = scrollView.contentOffset;
   CGSize contentSize = scrollView.contentSize;
   DDLogVerbose(@"scrollViewDidEndZooming: new content size = %f / %f ",
                contentSize.width, contentSize.height);
+*/
 
   // Big change here: This resets the scroll view's contentSize and
   // contentOffset, and also the PlayView's frame, bounds and transform
@@ -164,21 +223,21 @@
   scrollView.zoomScale = 1.0f;
   // Adjust the minimum and maximum zoom scale so that the user cannot zoom
   // in/out more than originally intended
-  scrollView.minimumZoomScale = scrollView.minimumZoomScale / scale;
-  scrollView.maximumZoomScale = scrollView.maximumZoomScale / scale;
+  scrollView.minimumZoomScale = newMinimumZoomScale;
+  scrollView.maximumZoomScale = newMaximumZoomScale;
   DDLogVerbose(@"scrollViewDidEndZooming: new minimumZoomScale = %f, maximumZoomScale = %f",
                scrollView.minimumZoomScale, scrollView.maximumZoomScale);
 
   // Re-apply some property values that were changed when the zoom scale was
   // reset to 1.0
-  scrollView.contentSize = contentSize;
-  [scrollView setContentOffset:contentOffset animated:NO];
-  self.playView.frame = CGRectMake(0, 0, contentSize.width, contentSize.height);
+  scrollView.contentSize = newContentSize;
+  [scrollView setContentOffset:newContentOffset animated:NO];
+  self.playView.frame = CGRectMake(0, 0, newContentSize.width, newContentSize.height);
 
   [self synchronizeZoomScale:self.playViewScrollView.zoomScale
             minimumZoomScale:self.playViewScrollView.minimumZoomScale
             maximumZoomScale:self.playViewScrollView.maximumZoomScale];
-  [self synchronizeContentOffset:contentOffset];
+  [self synchronizeContentOffset:newContentOffset];
   // At this point we should also update content size and frame changes. We
   // don't do so because PlayView already takes care of all this for us.
   self.playView.coordinateLabelsLetterViewScrollView.hidden = NO;
